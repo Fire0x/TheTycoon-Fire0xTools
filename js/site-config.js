29c9374abcd1bad05911f9f3630b1acde119a6bc
Fire0x <spocock222@yahoo.com>
2025-12-15 18:29:13 +1000

    all-in-one export/import and fixed owned number

--- js/site-config.js
+++ js/site-config.js
@@ -239,7 +239,7 @@ const VersionManager = {
         'checklist.html': '0.1.6',
         'VehicleDeliveries.html': '0.1.4',
         'education_timer.html': '0.1.0',
-        'apartment.html': '0.0.4'
+        'apartment.html': '0.0.5'
     },
     
     /**
